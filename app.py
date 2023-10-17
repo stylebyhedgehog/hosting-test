48154094ae6c2e659ee3d10cc26dcd9969cb42d6
fogdealer <iowork00@gmail.com>
2023-10-17 16:07:54 +0300

    init

--- app.py
+++ app.py
@@ -3,22 +3,22 @@ import time
 
 import flask
 # import os
-# import telebot
+import telebot
 # from flask_cors import CORS
 logging.info('App start')
-# API_TOKEN = os.environ.get("TELEBOT_TOKEN")
+API_TOKEN =  "6423402186:AAF5J0RrcoNO7xOMkjxQfZKn7aQdQ0IPxr0" #os.environ.get("TELEBOT_TOKEN")
 
-# WEBHOOK_HOST = os.environ.get("HOST_URL")
+WEBHOOK_HOST = "https://test-host-h6po.onrender.com/"
 WEBHOOK_PORT = 80  # 443, 80, 88 or 8443 (port need to be 'open')
 # WEBHOOK_LISTEN = os.environ.get("IP")  # In some VPS you may need to put here the IP addr
 #
-# WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-# WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
-#
-# logger = telebot.logger
-# telebot.logger.setLevel(logging.INFO)
-#
-# bot = telebot.TeleBot(API_TOKEN)
+WEBHOOK_URL_BASE = WEBHOOK_HOST #"https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL = WEBHOOK_HOST+API_TOKEN
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+bot = telebot.TeleBot(API_TOKEN)
 
 app = flask.Flask(__name__)
 
@@ -29,54 +29,52 @@ def index():
     return 'hello'
 
 
-@app.route("/test-messaging", methods=['POST'])
-def testhook():
+@app.route("/test-post", methods=['POST'])
+def testpost():
     if flask.request.headers.get('content-type') == 'application/json':
         return str(flask.request.get_data())
     else:
         flask.abort(403)
 
 # Process webhook calls
-# @app.route(WEBHOOK_URL_PATH, methods=['POST'])
-# def webhook():
-#     if flask.request.headers.get('content-type') == 'application/json':
-#         json_string = flask.request.get_data().decode('utf-8')
-#         update = telebot.types.Update.de_json(json_string)
-#         bot.process_new_updates([update])
-#         return ''
-#     else:
-#         flask.abort(403)
+@app.route(WEBHOOK_URL, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
 
 
-# @app.route("/test-messaging", methods=['POST'])
-# def testhook():
-#     if flask.request.headers.get('content-type') == 'application/json':
-#         bot.send_message(5891891154, str(flask.request.get_data()))
-#         return ''
-#     else:
-#         flask.abort(403)
+@app.route("/test-messaging", methods=['POST'])
+def testhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        bot.send_message(5891891154, str(flask.request.get_data()))
+        return ''
+    else:
+        flask.abort(403)
 
-# Handle '/start' and '/help'
-# @bot.message_handler(commands=['help', 'start'])
-# def send_welcome(message):
-#     bot.reply_to(message,
-#                  ("Hi there"))
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 ("Hi there"))
 
 
-# Handle all other messages
-# @bot.message_handler(func=lambda message: True, content_types=['text'])
-# def echo_message(message):
-#     bot.reply_to(message, message.text)
-#
-#
-# # Remove webhook, it fails sometimes the set if there is a previous webhook
-# bot.remove_webhook()
-#
-# time.sleep(1)
-#
-# # Set webhook
-# bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
-#                 # certificate=open(WEBHOOK_SSL_CERT, 'r')
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+time.sleep(1)
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL)
+                # certificate=open(WEBHOOK_SSL_CERT, 'r')
 
 # if __name__ == "__main__":
 #     app.run(port=80)
