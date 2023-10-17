3f8f74d3db53a29a5ac0de27cf174264b7b20c36
fogdealer <iowork00@gmail.com>
2023-10-17 16:33:28 +0300

    init

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ import telebot
 
 TOKEN = "6423402186:AAF5J0RrcoNO7xOMkjxQfZKn7aQdQ0IPxr0"
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+app = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
@@ -95,7 +95,7 @@ def echo_message(message):
     bot.reply_to(message, message.text)
 
 
-@server.route('/' + TOKEN, methods=['POST'])
+@app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
@@ -103,7 +103,7 @@ def getMessage():
     return "!", 200
 
 
-@server.route("/")
+@app.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://test-host-h6po.onrender.com/' + TOKEN)
